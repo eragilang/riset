0f282dac156dc1e44bd6b57eb7313b3ef4c9d125
egi muhammad <eragilang@gmail.com>
2022-10-01 19:23:03 +0700

    first commit

---
+++ vivet/routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/landing', function () {
+    return view('landing');
+});
+
+//Login Controller
+Route::post('loginact', 'LoginController@login');
