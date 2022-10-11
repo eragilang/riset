e837d3b59348c62384b5dfebefa88cc99c513579
xlobin <lobin.hoop@gmail.com>
2022-10-11 09:54:56 +0700

    fix: route landing page

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
 });
 
 Route::get('/login', function () {
     return view('login');
-});
-
-Route::get('/landing', function () {
-    return view('landing');
 });
 
 //Login Controller
